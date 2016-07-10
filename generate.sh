6494a5684e7fc056e95afb0a5e27417f5324e803
Barta Gábor <emoryy@gmail.com>
2016-07-10 20:56:37 +0200

    improved generate.sh

--- generate.sh
+++ generate.sh
@@ -3,43 +3,42 @@
 
 # add padding to text file: two spaces to the beginning and to the end of every line
 pad() {
-  sed -i -e 's/$/  /; s/^/  /;' $1
+  sed -i -e 's/^/  /; s/$/  /;' $1
 }
 
 # generate 3 text logo versions from a 1008x1008 png file
 generate() {
-  outputdir=textlogos/${1}
+  echo "Processing $1 file..."
+  filename=$(basename "$1")
+  filenamebase="${filename%.*}"
+  outputdir="textlogos/${filenamebase}"
   mkdir -p ${outputdir}
-  originalpng=pngs/${1}.png
+  originalpng="pngs/${filenamebase}.png"
 
-  bm2uns -c28x56 -p232 $originalpng > $outputdir/${1}_high.txt
-  pad $outputdir/${1}_high.txt
+  bm2uns -c28x56 -p232 $originalpng > $outputdir/${filenamebase}_high.txt
+  pad $outputdir/${filenamebase}_high.txt
 
   # make a 72x72 pixelated version with bm2uns
-  convert $originalpng -resize 72x72 ${1}_72x72.png
-  bm2uns -c2x4 -p232 ${1}_72x72.png > $outputdir/${1}_72x72.txt
-  pad $outputdir/${1}_72x72.txt
-  rm ${1}_72x72.png
+  convert $originalpng -resize 72x72 ${filenamebase}_72x72.png
+  bm2uns -c2x4 -p232 ${filenamebase}_72x72.png > $outputdir/${filenamebase}_72x72.txt
+  pad $outputdir/${filenamebase}_72x72.txt
+  rm ${filenamebase}_72x72.png
 
   # make a 36x36 pixelated version with gotermimg
-  convert $originalpng -resize 36x36 ${1}_36x36.png
-  gotermimg -u ${1}_36x36.png > $outputdir/${1}_36x36.txt
-  pad $outputdir/${1}_36x36.txt
-  rm ${1}_36x36.png
+  convert $originalpng -resize 36x36 ${filenamebase}_36x36.png
+  gotermimg -u ${filenamebase}_36x36.png > $outputdir/${filenamebase}_36x36.txt
+  pad $outputdir/${filenamebase}_36x36.txt
+  rm ${filenamebase}_36x36.png
 }
 
 if [[ -d $1 ]]; then
   files=${1%/}/*
   for file in $files
   do
-    echo "Processing $file file..."
-    filename=$(basename "$file")
-    filenamebase="${filename%.*}"
-    echo $filenamebase
-    generate $filenamebase
+    generate $file
   done
 elif [[ -f $1 ]]; then
-    echo "$1 is a file"
+    generate $1
 else
     echo "$1 is not valid"
     exit 1
